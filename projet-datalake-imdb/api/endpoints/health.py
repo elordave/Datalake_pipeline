ff1e08ee0a454fd8ab8552252321e3c7aa81a833
elordave <126555790+elordave@users.noreply.github.com>
2025-03-11 18:22:24 +0100

    commit2
    commit2

--- projet-datalake-imdb/api/endpoints/health.py
+++
@@ -1,7 +0,0 @@
-from flask import Blueprint, jsonify
-
-health_bp = Blueprint("health_bp", __name__)
-
-@health_bp.route("/", methods=["GET"])
-def health_check():
-    return jsonify({"status": "OK"}), 200
